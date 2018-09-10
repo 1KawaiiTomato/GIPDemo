94bbefca9b5ae8d7e364cc97d0615ba2e5c1986a
rik fabri <rik.fabri@hotmail.be>
2018-09-10 09:41:17 +0200

    Added the game class

---
+++ GIPDemo/game.cpp
@@ -0,0 +1,28 @@
+#include "game.h"
+
+
+
+void game::init()
+{
+}
+
+void game::startMain()
+{
+}
+
+void game::render()
+{
+}
+
+void game::draw()
+{
+}
+
+game::game()
+{
+}
+
+
+game::~game()
+{
+}
---
+++ GIPDemo/game.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+class game
+{
+private:
+
+public:
+	void init();
+	void startMain();
+	void render();
+	void draw();
+	game();
+	~game();
+};
+
