6da4298fe2743bbfe4bf72952475658507f7dc31
rik fabri <rik.fabri@hotmail.be>
2019-03-03 14:30:21 +0100

    Inheriting the animation interface

--- GIPDemo/Mob.cpp
+++ GIPDemo/Mob.cpp
@@ -4,16 +4,20 @@
 
 void Mob::render()
 {
+	al_draw_scaled_bitmap((*animation)[animationCount], 0, 0, 16, 16, ++x, y, 32, 32, 0);
 }
 
 void Mob::update()
 {
+	updateAnimation();
 }
 
 Mob::Mob()
 {
+	y = 395;
+	x = std::rand() % 100;
+	setAnimation(&Textures::getInstance().animations["frogIdle"], 6);
 }
-
 
 Mob::~Mob()
 {
--- GIPDemo/Mob.h
+++ GIPDemo/Mob.h
@@ -1,15 +1,17 @@
 #pragma once
-#include "MonsterManager.h"
+#include "Textures.h"
+#include "IAnimation.h"
+#include "allegro5/allegro.h"
 
-class Mob
+class Mob : public IAnimation
 {
 private:
 	float x, y;
 	ALLEGRO_BITMAP *Texture;
+	int bx, by;
 public:
 	virtual void render();
 	virtual void update();
 	Mob();
 	~Mob();
 };
-
